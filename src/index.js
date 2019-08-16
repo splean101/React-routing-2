de3f849cf4e5357c8dde5eb13436405d70b8aee9
splean101 <101splean@gmail.com>
2019-08-16 12:31:00 +0300

    cs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
+const adress = 'localhost:8080/';
+const names = ['Adam', 'Bob', 'Chris'];
 
+const App = (props) => {
+    const list = props.names.map((value, index) => {
+    return <li key={index}><NavLink to={{adress} + ':' + {value}}>second</NavLink></li>;
+});
+    return (
+        <ul>{list}</ul>
+    );
+};
 
-ReactDOM.render(<CalcForm />, document.getElementById("div1"));
+ReactDOM.render(<App adress={adress} names={names} />, document.getElementById('root'));
